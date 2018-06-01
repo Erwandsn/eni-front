f0aaae89936de1131588eab9fb9b9a6419abfe81
edesanogueira2017 <edesanogueira2017@43-UC26-15.ad.campus-eni.fr>
2018-06-01 11:46:12 +0200

    debut .js

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,36 @@
 
 
 $(function(){
-	
-}
+	$("#btnSearch").on('click', function(){	
+		var search = $('#iptSearch').val();
+		var lien ='http://www.omdbapi.com/';
+		$.ajax({
+			type: "GET",
+			url: 		lien,
+			dataType:   "json",
+			data :{ 
+				s : search,
+				apikey :"efdc2275",
+				type:"movie"
+			},
+			success:    function(info){
+				
+				if(info.Search != undefined){
+					tabFilm = info.Search;
+					$('#tabFilm tbody').html("");
+					for (let film of tabFilm){
+						var str = '<tr data-id='+film.imdbID+'>';
+						str+= '<td>'+ film.Year+'</td>';
+						str+= '<td>'+ film.Title+'</td>';
+						str+= '<td><img src="'+ film.Poster+'" width="200" ></td>';
+						str+= '<td>'+ getBtnAjout()+'</td>';
+						str += '</tr>';
+						$('#tabFilm').append(str);
+					}
+				}
+			}
+					
+		}).fail(function(){alert('error')});
+		
+	});
+});
